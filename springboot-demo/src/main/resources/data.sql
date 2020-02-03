28295238fd67a3ae11545451c3d617bb74ea3b31
Alchemy <Alchemy@192.168.1.206>
2020-02-03 02:34:38 -0800

    springdemo2

---
+++ springboot-demo/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into STUDENT(fName,lName,email)values('Sravani','Butari','abc@email.com');
+insert into STUDENT(fName,lName,email)values('a','b','cde@email.com');
+insert into STUDENT(fName,lName,email)values('c','d','pqr@email.com');
+insert into STUDENT(fName,lName,email)values('p','q','xyz@email.com');
+insert into STUDENT(fName,lName,email)values('m','n','web@email.com');
